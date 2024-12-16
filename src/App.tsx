8a72656773e31ac37a7bbd198f52261492fa5ebe
jinyoung joh <>
2024-12-16 12:02:00 +0900

    SL-250-3. Emotion 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import logo from './logo.svg'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 import './App.css'
+
+const bold = css`
+  font-weight: bold;
+`
+
+const contianerStyles = css`
+  background-color: black;
+  ${bold}
+`
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
+  ${bold}
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" css={contianerStyles}>
+      <Button>스타일 버튼</Button>
     </div>
   )
 }
